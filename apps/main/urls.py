48cf671fd12d2e041e0748c15eb3f29a60d3ede1
aliamani212 <ali.amani212@gmail.com>
2025-11-30 06:05:18 -0800

    qrq3r

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.main.views import index, median_blur_view
+from apps.main.views import index, median_blur_view, gaussian_blur_view
 
 app_name = 'main'
 urlpatterns = [
     path('', index, name='home'),
     path('median_blur_view/', median_blur_view, name='median_blur_view'),
+    path('gaussian_blur_view/', gaussian_blur_view, name='gaussian_blur_view'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -16,7 +16,7 @@ def index(request):
     return render(request, 'index.html')
 
 
-@csrf_exempt
+# @csrf_exempt
 def median_blur_view(request):
     if request.method != 'POST':
         return JsonResponse({"error": "فقط متد POST مجاز است."}, status=405)
@@ -36,6 +36,43 @@ def median_blur_view(request):
     # اعمال فیلتر median blur
     kesize = 21
     img_blurred = cv2.medianBlur(img, kesize)
+
+    # تبدیل به JPEG و سپس به base64 (برای ارسال در JSON)
+    success, buffer = cv2.imencode('.jpg', img_blurred)
+    if not success:
+        return JsonResponse({"error": "خطا در پردازش تصویر."}, status=500)
+
+    # تبدیل بایت‌ها به base64 برای ارسال در JSON
+    img_base64 = base64.b64encode(buffer).decode('utf-8')
+
+    return JsonResponse({
+        "processed_image": img_base64,
+        "message": "تصویر با موفقیت بلور شد."
+    })
+
+
+# @csrf_exempt
+def gaussian_blur_view(request):
+    if request.method != 'POST':
+        return JsonResponse({"error": "فقط متد POST مجاز است."}, status=405)
+
+    file = request.FILES.get("image")
+    if not file:
+        return JsonResponse({"error": "هیچ فایلی ارسال نشده است."}, status=400)
+
+    # خواندن بایت‌های تصویر آپلود شده
+    image_bytes = file.read()
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    if img is None:
+        return JsonResponse({"error": "فرمت تصویر نامعتبر است."}, status=400)
+
+    # اعمال فیلتر Gaussian blur
+    kesize = (23, 23)  # اندازه کرنل (باید فرد باشد)
+    sigma_x = 0  # انحراف استاندارد در جهت X (0 یعنی محاسبه خودکار)
+    sigma_y = 0  # انحراف استاندارد در جهت Y (0 یعنی محاسبه خودکار)
+    img_blurred = cv2.GaussianBlur(img, kesize, sigma_x, sigma_y)
 
     # تبدیل به JPEG و سپس به base64 (برای ارسال در JSON)
     success, buffer = cv2.imencode('.jpg', img_blurred)
